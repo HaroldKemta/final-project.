f7dc13ab9e8a0044d8aeb2a01c669f7d53c40370
Harold Kemta <hxk8928@mavs.uta.edu>
2025-04-01 15:08:28 -0500

    Create User.java

---
+++ User.java
@@ -0,0 +1,35 @@
+// import java collections framework 
+
+import java.util.ArrayList;  // used to store multiple bank accounts 
+import java.util.List; 
+
+
+public class User {
+
+  private String name;
+
+  private int userId;
+
+  private List<BankAccount> accounts; // list of bankAccount objust __ each user can have many 
+
+
+  public User( String name , int userId) {
+
+    this.name = name;
+
+    this.userId = UserId;
+
+    this.accounts = new ArrayList<> (); // creates emoty list of bank accounts for user 
+
+    public void  addAccount(BankAccount account) {
+
+      accounts.add(account);  // stores in user"s account list.
+    }
+
+  
+
+
+  
+
+  
+}
