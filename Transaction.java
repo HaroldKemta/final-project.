9e887d38afd4e0b3431141be40a1c3fb72240332
Harold Kemta <hxk8928@mavs.uta.edu>
2025-04-22 20:18:13 -0500

    Create Transaction.java

---
+++ Transaction.java
@@ -0,0 +1,22 @@
+// create transaction 
+
+// base case 
+
+import java.util.Date;
+
+public abstract class Transaction {
+    protected String type;
+    protected double amount;
+    protected Date date;
+    protected BankAccount account;
+
+    public Transaction(String type, double amount, BankAccount account) {
+        this.type = type;
+        this.amount = amount;
+        this.date = new Date();  // Current date and time of transaction 
+
+        this.account = account;
+    }
+
+    public abstract void execute();   // every subclass must define execute 
+}
